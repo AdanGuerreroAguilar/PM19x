bcb1c6054745fbb77a8cbf620db4e41d7afb0c09
AdanGuerreroAguilar <122043009@upq.edu.mx>
2025-05-16 15:34:28 -0600

    Ejercicio A
    .

---
+++ Practica1.js
@@ -0,0 +1,18 @@
+/*var nombre = "Armando";
+var edad = 25;
+
+nombre = "Ana Maria";
+
+var saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+*/
+
+//muestra en consola el contenido del saludo 
+
+let nombre = "Armando"; 
+const edad = 25;        
+
+nombre = "Ana Maria";   
+
+const saludo = `Hola, ${nombre}. Tienes ${edad} años.`; 
+
+console.log(saludo); // Muestra en consola el saludo
