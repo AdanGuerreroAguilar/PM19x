cf7227e82a22a5f5a79cecbd6b2fd1f74291104e
AdanGuerreroAguilar <122043009@upq.edu.mx>
2025-05-19 18:51:43 -0600

    Practica 2.a

---
+++ Practica2.js
@@ -0,0 +1,8 @@
+const persona = {
+    nombre: "Adan Guerrero",
+    edad: 22,
+    ciudad: "Querétaro"
+  };
+  const { nombre, edad, ciudad } = persona;
+  console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
+ 
