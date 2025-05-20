22e561825e9f986e29508d0da3182a1462c5fec0
AdanGuerreroAguilar <122043009@upq.edu.mx>
2025-05-20 10:52:32 -0600

    Practica2.b

--- Practica2.js
+++ Practica2.js
@@ -1,4 +1,4 @@
-const persona = {
+/*const persona = {
     nombre: "Adan Guerrero",
     edad: 22,
     ciudad: "Querétaro"
@@ -6,3 +6,19 @@ const persona = {
   const { nombre, edad, ciudad } = persona;
   console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
  
+
+  ----------------
+  Ejercicio b
+*/
+const productos = [
+  { nombre: "Laptop", precio: 12000 },
+  { nombre: "Mouse", precio: 250 },
+  { nombre: "Teclado", precio: 750 },
+  { nombre: "Monitor", precio: 3000 }
+];
+
+const nombresFiltrados = productos.filter(producto => producto.precio > 1000).map(producto => producto.nombre);
+
+console.log(nombresFiltrados);
+
+
